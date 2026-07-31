838adbb3cb6911ba87ad874c886d9741c1a08ca1
rcspam <10021906+rcspam@users.noreply.github.com>
2026-07-31 13:29:03 +0200

    Add rcspam alongside the existing copyright holder
    Requested in review.

--- contents/ui/js/tempColors.js
+++ contents/ui/js/tempColors.js
@@ -1,5 +1,6 @@
 /*
  * Copyright 2026  Petar Nedyalkov
+ * Copyright 2026  rcspam
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
